e198d36467e1f994cf8499ff3475e9ba7084cfd6
MiguelMadero <me@miguelmadero.com>
2009-09-06 21:04:52 +1000

    Added tests for Binding and Implicit Binding

---
+++ Ninject.Moq.Tests/BindTests.cs
@@ -0,0 +1,28 @@
+﻿using Moq;
+using Ninject.Tests.Fakes;
+using Xunit;
+using Xunit.Should;
+
+namespace Ninject.Moq.Tests
+{
+    public class BindContext
+    {
+        protected readonly StandardKernel kernel;
+
+        public BindContext()
+        {
+            kernel = new StandardKernel();
+        }
+    }
+
+    public class BindTests : BindContext
+    {
+        [Fact]
+        public void ShouldCreateMock()
+        {
+            kernel.Bind<Samurai>().ToMock();
+            var samurai = kernel.Get<Samurai>();
+            samurai.Weapon.ShouldBeInstanceOf(new Mock<IWeapon>().Object.GetType());
+        }
+    }
+}
---
+++ Ninject.Moq.Tests/ImplicitBindingTests.cs
@@ -0,0 +1,28 @@
+﻿using Moq;
+using Ninject.Tests.Fakes;
+using Xunit;
+using Xunit.Should;
+
+namespace Ninject.Moq.Tests
+{
+    public class ImplicitBindingContext
+    {
+        protected readonly MockingKernel kernel;
+
+        public ImplicitBindingContext()
+        {
+            kernel = new MockingKernel();
+        }
+    }
+
+    public class ImplicitBindingTests : ImplicitBindingContext
+    {
+        [Fact]
+        public void DependenciesAreResolvedUsingMockedObjects()
+        {
+            var samuari = kernel.Get<Samurai>();
+            samuari.Weapon.ShouldNotBeNull();
+            samuari.Weapon.ShouldBeInstanceOf(new Mock<IWeapon>().Object.GetType());
+        }
+    }
+}
